7cae00660004289a1c78b4bb663300641e40031c
moniyama <monicasayama@gmail.com>
2019-08-29 15:50:47 -0300

    com avaliação de históricogit add src/index.js git add src/index.js ! YAY!

--- src/index.js
+++ src/index.js
@@ -1,39 +1,67 @@
-document.getElementById('button').addEventListener("click", tweet)
+
+
+document.getElementById('button').addEventListener("click", newTweet)
 document.getElementById('button').addEventListener("click", clean)
-  let tweetHistory
+// let tweetHistory
 // let hour = [];
 // let d = new Date();
 // let time = String(d.getHours()) + ":" + d.getMinutes();
+let history = [];
+let historico;
 
-function tweet () {
-
-  if(tweetHistory == null) {
-    tweetHistory = [];
-    console.log('tweetHistory null');
-
-  } else {
-    tweetHistory = JSON.parse(localStorage.getItem('tweet')); // a array recupera oq está no localStorage;
-    console.log('tweetHistory JSON');
-    }
-
-  tweetHistory.unshift(document.getElementById('tweet').value); // adiciona na array
-  // hour.push(time);
-  let tweetHistoryJSON = JSON.stringify(tweetHistory); // transforma a array em obj JSON
-
-  localStorage.setItem('tweet', tweetHistoryJSON);  // coloca no localStorage a array obj JSON
-  // localStorage.setItem('hour', time);
-  console.log(localStorage);
-  // console.log(hour)
-print();
+if(!localStorage.getItem("tweet")) {
+  historico = [];
+  console.log('inicio sem historico');
+} else {
+  console.log('inicio tem historico');
+  print();
 }
 
+
+
+function newTweet() {
+  history = historico;
+  history.unshift(document.getElementById('tweet').value);     // adiciona na array o input
+  console.log(history);
+  historyJSON = JSON.stringify(history);        // transforma a array em obj JSON
+  localStorage.setItem('tweet', historyJSON)
+
+print()
+}
+
+// function tweet () {
+//
+//   if(tweetHistory == null) {
+//     tweetHistory = [];
+//     console.log('tweetHistory null');
+//
+//   } else {
+//     tweetHistory = JSON.parse(localStorage.getItem('tweet')); // a array recupera oq está no localStorage;
+//     console.log('tweetHistory JSON');
+//     }
+//
+//   tweetHistory.unshift(document.getElementById('tweet').value); // adiciona na array
+//   // hour.push(time);
+//   let tweetHistoryJSON = JSON.stringify(tweetHistory); // transforma a array em obj JSON
+//
+//   localStorage.setItem('tweet', tweetHistoryJSON);  // coloca no localStorage a array obj JSON
+//   // localStorage.setItem('hour', time);
+//   console.log(localStorage);
+//   // console.log(hour)
+// print();
+// }
+//
+
 function print() {
-  let tweetHistoryPrint = tweetHistory.map(function (tweetHistory) {
-    return '<li>' + tweetHistory + '</li>'
+  historico = JSON.parse(localStorage.getItem('tweet')); // recupera oq está no localStorage(JSON) e transf. em array;
+  let historyPrint = historico.map(function (historico) {
+    return '<li>' + historico + '</li>'
   }).join("");
 
   document.getElementById('historico').innerHTML =
-  `<ul> ${tweetHistoryPrint} </ul> `
+  `<ul> ${historyPrint} </ul> `
+
+
 }
 
 function clean () {
