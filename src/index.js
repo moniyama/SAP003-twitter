8ba7f31c1c33db2f7f0baff9fb58bceba340fa69
moniyama <monicasayama@gmail.com>
2019-08-29 13:27:28 -0300

    printando em lista

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
-document.getElementById('button').onclick = function tweet () {
-  event.preventDefault();
-let d = new Date();
-    document.getElementById('historico').innerHTML =
-    `<p> ${(document.getElementById('tweet').value)}
-    <br> ${d.getHours()}:${d.getMinutes()}`
+document.getElementById('button').addEventListener("click", tweet)
+document.getElementById('button').addEventListener("click", clean)
+  let tweetHistory
+// let hour = [];
+// let d = new Date();
+// let time = String(d.getHours()) + ":" + d.getMinutes();
+
+function tweet () {
+
+  if(tweetHistory == null) {
+    tweetHistory = [];
+    console.log('tweetHistory null');
+
+  } else {
+    tweetHistory = JSON.parse(localStorage.getItem('tweet')); // a array recupera oq está no localStorage;
+    console.log('tweetHistory JSON');
+    }
+
+  tweetHistory.unshift(document.getElementById('tweet').value); // adiciona na array
+  // hour.push(time);
+  let tweetHistoryJSON = JSON.stringify(tweetHistory); // transforma a array em obj JSON
+
+  localStorage.setItem('tweet', tweetHistoryJSON);  // coloca no localStorage a array obj JSON
+  // localStorage.setItem('hour', time);
+  console.log(localStorage);
+  // console.log(hour)
+print();
+}
+
+function print() {
+  let tweetHistoryPrint = tweetHistory.map(function (tweetHistory) {
+    return '<li>' + tweetHistory + '</li>'
+  }).join("");
+
+  document.getElementById('historico').innerHTML =
+  `<ul> ${tweetHistoryPrint} </ul> `
+}
+
+function clean () {
+  document.getElementById('form').reset();
 
 }
